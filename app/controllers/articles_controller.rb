b3cb91a2cf88eff47f710885688c2f4a1abfcd5d
tsu <>
2025-09-01 21:17:09 +0900

    #16

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [ :show, :edit, :update ]
 
     def index
+        raise StandardError
         @articles=Article.all
     end
 
@@ -15,9 +16,9 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-          redirect_to article_path(@article), notice: "保存できたよ"
+          redirect_to article_path(@article), notice: '保存できたよ'
         else
-            flash.now[:error] = "保存に失敗しました"
+            flash.now[:error] = '保存に失敗しました'
             render :new
         end
     end
@@ -27,9 +28,9 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)
-            redirect_to article_path(@article), notice: "更新できました"
+            redirect_to article_path(@article), notice: '更新できました'
         else
-              flash.now[:error] = "更新できませんでした"
+              flash.now[:error] = '更新できませんでした'
               render :edit, status: :unprocessable_entity
         end
     end
@@ -37,14 +38,13 @@ class ArticlesController < ApplicationController
     def destroy
         article=Article.find(params[:id])
         article.destroy!
-        redirect_to root_path, status: :see_other, notice: "削除に成功しました"
+        redirect_to root_path, status: :see_other, notice: '削除に成功しました'
     end
 
     private
     def article_params
       params.require(:article).permit(:title, :content)
     end
-
 
     def set_article
         @article=Article.find(params[:id])
